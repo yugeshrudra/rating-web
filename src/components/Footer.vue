65de1a8a76852762db5f6c7442a151074d596c86
brianstinehart <brian.stinehart22@gmail.com>
2022-03-25 11:25:09 +1100

    spacing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,11 +24,20 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Red Hat OpenShift on AWS Workshop
+        -
       </div>
       <div class="col-lg-6">
       </div>
     </div>
+    <div class="row at-row flex-center flex-middle">
+      <div class="col-lg-6">
+      </div>
+      <div class="col-lg-12 credits">
+        Red Hat OpenShift on AWS Workshop
+      </div>
+      <div class="col-lg-6">
+      </div>
+    </div>    
     <div class="row at-row flex-center flex-middle credits-tag">
       <div class="col-lg-6">
       </div>
